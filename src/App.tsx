7a450cfbe73195d1384a507fb1b1a8081c5c850b
akhilggp <akhilgdp@gmail.com>
2024-09-28 18:46:00 -0700

    OnClick, UseState, Props, Components, Buttons, Alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import Message from './Message'
+import ListGroup from './components/ListGroup'
+import PropsListGroup from './components/PropsListGroup'
+import Button from './components/Button'
+import Alert from './components/Alert'
+import { Fragment, useState } from 'react';
 
 function App() {
-    return <div><Message /></div>;
+    const list= 3;
+    const items = ['Vij', 'Seattle', 'Baltimore', 'Virginia']
+    const [onVisibility, setOnVisibility] = useState(false);
+    if (list === 0)
+        return <div><Message /></div>;
+    else if (list === 1)
+        return (<div><ListGroup /></div>);
+    else if (list === 2)
+        return <div><PropsListGroup items={items} headings="Props Cities"/></div>;
+    return (
+        <>
+        {onVisibility && <Alert onClose={()=> setOnVisibility(false)}> My Alert </Alert>}
+        <Button onClick={()=> setOnVisibility(true)}> My Button </Button>
+        </>
+        );
     }
-export default Message
+export default App;
---
+++ src/components/PropsListGroup.tsx
@@ -0,0 +1,26 @@
+import { Fragment, useState } from 'react';
+
+interface Props {
+    items: string[];
+    headings: string;
+    }
+function PropsListGroup({items, headings}: Props){
+
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    return (
+        <>
+            <h1> {headings} </h1>
+            <ul className="list-group">
+                {items.map((item,index)=>
+                    <li
+                    className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+                    key={item}
+                    onClick = {()=>setSelectedIndex(index)}
+                    >{item}</li>
+                )}
+            </ul>
+        </>
+        );
+    }
+export default PropsListGroup;
